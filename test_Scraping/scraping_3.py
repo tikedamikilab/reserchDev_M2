15019a4bad83a1eb09c0dcb42c9cd0c44cbada53
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-04-24 06:47:39 +0900

    scraping pandas 別にこれでもできる

--- test_Scraping/scraping_3.py
+++ test_Scraping/scraping_3.py
@@ -1,25 +1,25 @@
 import pandas as pd
 from datetime import datetime as dt
+import time
 
 #url = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL&.tsrc=fin-srch'
-url = 'https://codeforces.com/problemset/'
+baseurl = 'https://codeforces.com/problemset/'
 status = "451"
 problem = "A"
 page = str(1)
 
-url = url + "status/" + status + "/problem/" + problem + "/page/" + page
-print(url)
-#url = "https://codeforces.com/problemset/status/451/problem/A/page/1"
-data = pd.read_html(url, header = 0)
-data[0].dropna(inplace = True)
+for i in range(1,10):
+    page = str(i)
+    url = baseurl + "status/" + status + "/problem/" + problem + "/page/" + page
+    print(url)
 
-#print(data[0]["When"].head())
+    data = pd.read_html(url, header = 0)
+    data[0].dropna(inplace = True)
 
-data[0]["datetime"] = [dt.strptime(i, '%b/%d/%Y %H:%M') for i in data[0]["When"]]
-print(data[0]["datetime"].head())
-
-data[0].to_csv("status"+ status + "_problem" + problem + ".csv", header=False, mode='a')
+    data[0]["datetime"] = [dt.strptime(i, '%b/%d/%Y %H:%M') for i in data[0]["When"]]
+    data[0].to_csv("status"+ status + "_problem" + problem + ".csv", header=False, mode='a')
 
 #data[0].set_index("Date2", inplace=True)
+#print(data[0]["datetime"].head())
 #string_date_1 = 'Apr 23, 2020'
 #print(dt.strptime(string_date_1, '%b %d, %Y'))
